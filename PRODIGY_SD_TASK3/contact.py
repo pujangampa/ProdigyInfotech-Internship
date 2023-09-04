2506e16f6be3c0dca96b2a01e2294e8c410d12e1
Pujan <130989697+pujangampa@users.noreply.github.com>
2023-09-04 17:53:09 +0530

    Create contact.py

---
+++ PRODIGY_SD_TASK3/contact.py
@@ -0,0 +1,96 @@
+import json
+
+CONTACTS_FILE = "contacts.json"
+
+
+def load_contacts():
+    try:
+        with open(CONTACTS_FILE, "r") as file:
+            return json.load(file)
+    except FileNotFoundError:
+        return {}
+
+
+def save_contacts(contacts):
+    with open(CONTACTS_FILE, "w") as file:
+        json.dump(contacts, file)
+
+
+def add_contact(contacts, name, phone, email):
+    if name in contacts:
+        print("Contact already exists.")
+    else:
+        contacts[name] = {"phone": phone, "email": email}
+        save_contacts(contacts)
+        print(f"Contact '{name}' added successfully.")
+
+
+def delete_contact(contacts, name):
+    if name in contacts:
+        del contacts[name]
+        save_contacts(contacts)
+        print(f"Contact '{name}' deleted successfully.")
+    else:
+        print("Contact not found.")
+
+
+def search_contact(contacts, name):
+    if name in contacts:
+        contact = contacts[name]
+        print(f"Name: {name}")
+        print(f"Phone: {contact['phone']}")
+        if 'email' in contact:
+            print(f"Email: {contact['email']}")
+    else:
+        print("Contact not found.")
+
+
+def list_contacts(contacts):
+    if contacts:
+        print("Contacts:")
+        for name in contacts:
+            print(name)
+    else:
+        print("No contacts found.")
+
+
+def main_menu():
+    contacts = load_contacts()
+    while True:
+        print("========================================")
+        print("\tContact Management System")
+        print("========================================\n")
+        print("\t 1. Add Contact\n")
+        print("\t 2. List Contact\n")
+        print("\t 3. Search Contact\n")
+        print("\t 4. Delete Contacts\n")
+        print("\t 5. Exit\n")
+
+        choice = input("\t Enter your choice (1-5): ")
+        if choice == "1":
+            name = input("\t Enter name: ")
+            phone = input("\t Enter phone number: ")
+            email = input("\t Enter email address: ")
+            add_contact(contacts, name, phone, email)
+        elif choice == "2":
+            list_contacts(contacts)
+        elif choice == "3":
+            if contacts:
+                name = input("Enter name: ")
+                search_contact(contacts, name)
+            else:
+                print("No contacts found.")
+        elif choice == "4":
+            if contacts:
+                name = input("Enter name: ")
+                delete_contact(contacts, name)
+            else:
+                print("No contacts found.")
+        elif choice == "5":
+            break
+        else:
+            print("Invalid choice. Please try again.")
+
+
+if __name__ == "__main__":
+    main_menu()
